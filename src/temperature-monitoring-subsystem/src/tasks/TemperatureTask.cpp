eb0606cde764ff5a0335621599cec8d67ba73383
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-15 17:36:58 +0100

    Added temperature task with wifi connection

---
+++ src/temperature-monitoring-subsystem/src/tasks/TemperatureTask.cpp
@@ -0,0 +1,41 @@
+#include "tasks/TemperatureTask.hpp"
+#include <Arduino.h>
+#include <WiFi.h>
+
+#define SSID ""
+#define PASSWORD ""
+#define TEMPERATURE_SENSOR_PIN 1
+#define RED_LED_PIN 2
+#define NO_ERROR_LED_PIN 3
+
+TemperatureTask::TemperatureTask(int period) {
+    this->temperatureSensor = new TempSensor(TEMPERATURE_SENSOR_PIN);
+    this->errorLed = new Led(RED_LED_PIN);
+    this->noErrorLed = new Led(NO_ERROR_LED_PIN);
+    this->temperature = this->temperatureSensor->getTemperature();
+    this->setPeriod(period);
+    this->networkNotOk();
+    WiFi.begin(SSID, PASSWORD);
+}
+
+void TemperatureTask::tick() {
+    this->temperature = this->temperatureSensor->getTemperature();
+    if(WiFi.status() != WL_CONNECTED) {
+        this->networkNotOk();
+        WiFi.begin(SSID, PASSWORD);        
+    } else {
+        this->networkOk();
+        Serial.println("connesso");
+        //send in mqtt
+    }
+}
+
+void TemperatureTask::networkOk() {
+    this->errorLed->switchOff();
+    this->noErrorLed->switchOn();
+}
+
+void TemperatureTask::networkNotOk() {
+    this->errorLed->switchOn();
+    this->noErrorLed->switchOff();
+}
