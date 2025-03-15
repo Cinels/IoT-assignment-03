d5a927050b32ff8032cd88add35992c43c8f633d
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-15 14:50:43 +0100

    Improved temperature sensor class by adding constants

--- src/temperature-monitoring-subsystem/src/devices/TempSensor.cpp
+++ src/temperature-monitoring-subsystem/src/devices/TempSensor.cpp
@@ -1,13 +1,17 @@
 #include <Arduino.h>
 #include "devices/TempSensor.hpp"
 
+#define TENSION_ZERO_DEGREES 0.5
+#define STEP_VOLT_PER_DEGREE 0.01
+#define ANALOGUE_PRECISION 0.0032
+
 TempSensor::TempSensor(int pin) {
     this->pin = pin;
     pinMode(this->pin, INPUT);
 }
 
 float TempSensor::getTemperature() {
-    int x = analogRead(this->pin);
-    float temp = (((float)x * 0.00488) - 0.5) / 0.01;
-    return temp;
+    int value = analogRead(pin);
+    float valueInCelsius = ((value * ANALOGUE_PRECISION) - TENSION_ZERO_DEGREES) / STEP_VOLT_PER_DEGREE;
+    return valueInCelsius;
 }
