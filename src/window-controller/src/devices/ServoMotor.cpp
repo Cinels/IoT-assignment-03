acfddf8c23d46c0c23caa428c662c866f889a57c
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-16 18:22:00 +0100

    Refactored servo motor class

--- src/window-controller/src/devices/ServoMotor.cpp
+++ src/window-controller/src/devices/ServoMotor.cpp
@@ -9,27 +9,27 @@ ServoMotor::ServoMotor(int pin) {
     this->pin = pin;
     this->on();
     delay(MOVING_TIME);
-    motor.write(DEFAULT_POSITION);
+    this->motor.write(DEFAULT_POSITION);
     delay(MOVING_TIME);
     this->off();
 }
 
 void ServoMotor::on(){
-  motor.attach(this->pin);
+  this->motor.attach(this->pin);
 }
 
 void ServoMotor::setPosition(int angle){
   this->on();
   delay(MOVING_TIME);
-  motor.write(angle);
+  this->motor.write(angle);
   delay(MOVING_TIME);
   this->off();
 }
 
 void ServoMotor::off(){
-  motor.detach();    
+  this->motor.detach();    
 }
 
 int ServoMotor::getPosition() {
-  return motor.read();
+  return this->motor.read();
 }
