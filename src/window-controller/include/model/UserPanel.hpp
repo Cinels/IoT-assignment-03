0d13837272ab029a746076e71c7c69e6bf193035
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-19 19:45:39 +0100

    Completed user panel class

--- src/window-controller/include/model/UserPanel.hpp
+++ src/window-controller/include/model/UserPanel.hpp
@@ -1,11 +1,35 @@
 #ifndef __USER_PANEL__
 #define __USER_PANEL__
 
+#include "devices/Button.hpp"
+#include "devices/Display.hpp"
+#include "devices/Potentiometer.hpp"
+
+/// @brief Class that represents the user panel.
 class UserPanel{
 private:
-    /* data */
+    Button* button;
+    Display* display;
+    Potentiometer* potentiometer;
 public:
-    UserPanel(/* args */);
+    /// @brief Constructor of the class.
+    UserPanel();
+    
+    /// @brief Returns the value of opening in manual mode.
+    /// @return the percentage of opening.
+    int getWindowManualOpening();
+    
+    /// @brief Displays the window opening.
+    /// @param opening the percentage of opening.
+    void displayWindowOpening(int opening);
+
+    /// @brief Displays the mode.
+    /// @param mode the mode to display.
+    void displayMode(int mode);
+
+    /// @brief Displays the temperature.
+    /// @param temperature the temperature to display.
+    void displayTemperature(float temperature);
 };
 
 #endif
--- src/window-controller/src/model/UserPanel.cpp
+++ src/window-controller/src/model/UserPanel.cpp
@@ -1,1 +1,38 @@
 #include "model/UserPanel.hpp"
+#include <Arduino.h>
+
+#define POTENTIOMETER_PIN A0
+#define BUTTON_PIN 4
+
+UserPanel::UserPanel() {
+    this->button = new Button(BUTTON_PIN);
+    this->potentiometer = new Potentiometer(POTENTIOMETER_PIN);
+    this->display = new Display();
+    this->display->init();
+    this->button->attachInterrupt();
+}
+
+int UserPanel::getWindowManualOpening() {
+    return this->potentiometer->mappedValue(0, 100);
+}
+
+void UserPanel::displayWindowOpening(int opening) {
+    this->display->setText(1, 0, "Window opening: ");
+    this->display->setText(1, 16, String(opening));
+    this->display->setText(1, 19, "%");
+}
+
+void UserPanel::displayMode(int mode) {
+    this->display->setText(2, 0, "Mode: ");
+    if (mode == 1) {
+        this->display->setText(2, 6, "Automatic");
+    } else {
+        this->display->setText(2, 6, "Manual");
+    }
+}
+
+void UserPanel::displayTemperature(float temperature) {
+    this->display->setText(3, 0, "Temperature: ");
+    this->display->setText(3, 13, String(temperature));
+    this->display->setText(3, 18, "°C");
+}
