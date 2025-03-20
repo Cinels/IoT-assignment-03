25816a601bc1cfb1c59020e42f150dc820863ba4
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-20 15:19:03 +0100

    Added main implementation

--- src/window-controller/src/main.cpp
+++ src/window-controller/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include "tasks/Scheduler.hpp"
+#include "tasks/CommunicationTask.hpp"
+#include "tasks/WindowTask.hpp"
+#include "model/SystemInformations.hpp"
+#include "model/Window.hpp"
+#include "model/UserPanel.hpp"
 
-// put function declarations here:
-int myFunction(int, int);
+SystemInformations* systemInformations;
+Window* window;
+UserPanel* userPanel;
+Scheduler* scheduler;
+WindowTask* windowTask;
+CommunicationTask* communicationTask;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  systemInformations = new SystemInformations();
+  window = new Window(3);
+  userPanel = new UserPanel(systemInformations);
+  scheduler = new Scheduler();
+  windowTask = new WindowTask(userPanel, window, systemInformations);
+  communicationTask = new CommunicationTask();
+
+  scheduler->init(150);
+  windowTask->init(150);
+  communicationTask->init(300);
+
+  scheduler->addTask(windowTask);
+  scheduler->addTask(communicationTask);
+
+  Serial.println("Setup done!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  scheduler->schedule();
 }
