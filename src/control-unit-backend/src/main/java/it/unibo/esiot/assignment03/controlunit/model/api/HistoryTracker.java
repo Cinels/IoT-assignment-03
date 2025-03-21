83111e066e07dfc79d47284d4e889af3e7313877
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-21 14:42:41 +0100

    Added history tracker class

---
+++ src/control-unit-backend/src/main/java/it/unibo/esiot/assignment03/controlunit/model/api/HistoryTracker.java
@@ -0,0 +1,44 @@
+package it.unibo.esiot.assignment03.controlunit.model.api;
+
+import java.util.List;
+
+/**
+ * Interface for a class that keeps track of a history of values and provides.
+ */
+public interface HistoryTracker {
+    
+    /**
+     * Returns the last N values added to the history.
+     * @return a list of the last N values added to the history.
+     */
+    List<Float> getHistory();
+
+    /**
+     * Adds a value to the history.
+     * @param value the value to add to the history.
+     */
+    void addValue(float value);
+
+    /**
+     * Clears the history.
+     */
+    void clearHistory();
+
+    /**
+     * Returns the average of the values in the history.
+     * @return the average of the values in the history.
+     */
+    float getAverage();
+
+    /**
+     * Returns the minimum value in the history.
+     * @return the minimum value in the history.
+     */
+    float getMin();
+
+    /**
+     * Returns the maximum value in the history.
+     * @return the maximum value in the history.
+     */
+    float getMax();
+}
---
+++ src/control-unit-backend/src/main/java/it/unibo/esiot/assignment03/controlunit/model/impl/HistoryTrackerImpl.java
@@ -0,0 +1,69 @@
+package it.unibo.esiot.assignment03.controlunit.model.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unibo.esiot.assignment03.controlunit.model.api.HistoryTracker;
+
+/**
+ * Class that keeps track of a history of values and provides.
+ */
+public class HistoryTrackerImpl implements HistoryTracker {
+
+    final static private int HISTORY_SIZE = 100;
+    final private List<Float> history;
+    private float sum;
+    private float min;
+    private float max;
+
+    /**
+     * Creates a new instance of HistoryTrackerImpl.
+     */
+    public HistoryTrackerImpl() {
+        this.history = new ArrayList<>();
+    }
+
+    @Override
+    public List<Float> getHistory() {
+        return List.copyOf(this.history);
+    }
+
+    @Override
+    public void addValue(float value) {
+        this.history.add(value);
+        this.sum += value;
+        if (this.history.size() > HISTORY_SIZE) {
+            this.sum -= this.history.remove(0);
+        }
+        if (this.history.isEmpty() || value < this.min) {
+            this.min = value;
+        }
+        if (this.history.isEmpty() || value > this.max) {
+            this.max = value;
+        }
+    }
+
+    @Override
+    public void clearHistory() {
+        this.history.clear();
+        this.sum = 0;
+        this.min = 0;
+        this.max = 0;
+    }
+
+    @Override
+    public float getAverage() {
+        return this.sum / (float) this.history.size();
+    }
+
+    @Override
+    public float getMin() {
+        return this.min;
+    }
+
+    @Override
+    public float getMax() {
+        return this.max;
+    }
+    
+}
