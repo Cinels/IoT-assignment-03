5886acac42ba9d8ce3e05e5b12e04bc31b34b8b3
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-21 13:12:17 +0100

    Added dashboard workspace, initializing the java project with gradle

---
+++ src/dashboard-frontend/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    // Apply the java plugin to add support for Java
+    java
+
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
+    application
+}
+
+repositories { // Where to search for dependencies
+    mavenCentral()
+}
+
+dependencies {
+    // Suppressions for SpotBugs
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.6")
+
+    // Maven dependencies are composed by a group name, a name and a version, separated by colons
+    implementation("com.omertron:API-OMDB:1.5")
+    implementation("org.jooq:jool:0.9.15")
+
+    /*
+     * Simple Logging Facade for Java (SLF4J) with Apache Log4j
+     * See: http://www.slf4j.org/
+     */
+    val slf4jVersion = "2.0.16"
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    // Logback backend for SLF4J
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.8")
+
+    // JUnit API and testing engine
+    val jUnitVersion = "5.11.1"
+    // when dependencies share the same version, grouping in a val helps to keep them in sync
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("it.unibo.oesiot.assignment03.dashboard.App")
+}
+
---
+++ src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/App.java
@@ -0,0 +1,7 @@
+package it.unibo.esiot.assignment03.dashboard;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
