6ff647d0baef4437a50ded6a3a6b3b0625da7e2b
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-22 23:48:51 +0100

    Added jssc dependency in gradle build file

--- src/control-unit-backend/build.gradle.kts
+++ src/control-unit-backend/build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
     implementation("com.omertron:API-OMDB:1.5")
     implementation("org.jooq:jool:0.9.15")
 
+    // https://mvnrepository.com/artifact/org.scream3r/jssc
+    implementation("org.scream3r:jssc:2.8.0")
+
     /*
      * Simple Logging Facade for Java (SLF4J) with Apache Log4j
      * See: http://www.slf4j.org/
