a1535f31b4bcbb1ed24ded9b348606bbd45c1ffe
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-29 20:21:30 +0100

    Added empty dashboard communication class

---
+++ src/control-unit-backend/src/main/java/it/unibo/esiot/assignment03/controlunit/communication/impl/DashboardCommunicationImpl.java
@@ -0,0 +1,10 @@
+package it.unibo.esiot.assignment03.controlunit.communication.impl;
+
+import it.unibo.esiot.assignment03.controlunit.communication.api.DashboardCommunication;
+
+/**
+ * 
+ */
+public class DashboardCommunicationImpl implements DashboardCommunication {
+
+}
