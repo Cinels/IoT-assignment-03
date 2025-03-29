1d507117191fc89ebbde9c568f5232d985fde1ab
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-29 19:49:49 +0100

    Added empty communication class

---
+++ src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/communication/ControlUnitCommunication.java
@@ -0,0 +1,8 @@
+package it.unibo.esiot.assignment03.dashboard.communication;
+
+/**
+ * Interface for the communication between the dashboard and the control unit.
+ */
+public interface ControlUnitCommunication {
+
+}
---
+++ src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/communication/ControlUnitCommunicationImpl.java
@@ -0,0 +1,8 @@
+package it.unibo.esiot.assignment03.dashboard.communication;
+
+/**
+ * Implementation of {@link ControlUnitCommunication}.
+ */
+public final class ControlUnitCommunicationImpl implements ControlUnitCommunication {
+
+}
