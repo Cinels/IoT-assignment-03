396186f2f87aa4f8fe77e418f3ae80176737018e
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-30 19:09:57 +0200

    Added chart drawing and added its dependency

--- src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/gui/HistoryBar.java
+++ src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/gui/HistoryBar.java
@@ -1,6 +1,15 @@
 package it.unibo.esiot.assignment03.dashboard.gui;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.esiot.assignment03.dashboard.controller.api.DashboardController;
@@ -10,8 +19,13 @@ import it.unibo.esiot.assignment03.dashboard.controller.api.DashboardController;
  */
 public final class HistoryBar extends JPanel {
     private static final long serialVersionUID = 1L;
+    private static final int WIDTH = 1000;
+    private static final int HEIGHT = 700;
+    private static final float MILLIS = 1000f;
 
     private final transient DashboardController controller;
+    private final long initTime;
+    private XYSeries series;
 
     /**
      * Creates a new history graph panel.
@@ -24,6 +38,41 @@ public final class HistoryBar extends JPanel {
     )
     public HistoryBar(final DashboardController controller) {
         this.controller = controller;
-        this.controller.setOpeningFromDashboard(0);
+        this.initTime = System.currentTimeMillis();
+        final var chart = new ChartPanel(this.createChart(this.createDataset()));
+        chart.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.add(chart);
+        this.updateDataset();
+    }
+
+    private JFreeChart createChart(final XYSeriesCollection dataset) {
+        return ChartFactory.createXYLineChart(
+            "History",
+            "Time",
+            "Temperature",
+            dataset,
+            PlotOrientation.VERTICAL,
+            false,
+            false,
+            false
+        );
+    }
+
+    private XYSeriesCollection createDataset() {
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        series = new XYSeries("Temperature");
+        dataset.addSeries(series);
+        return dataset;
+    }
+
+    /**
+     * Updates the chart dataset.
+     */
+    public void updateDataset() {
+        final var history = this.controller.getTemperatureHistory();
+        series.clear();
+        for (final var pair : history) {
+            series.add((pair.getX() - initTime) / MILLIS, pair.getY());
+        }
     }
 }
