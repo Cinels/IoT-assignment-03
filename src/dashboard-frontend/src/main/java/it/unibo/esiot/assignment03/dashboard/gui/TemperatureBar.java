15299f88f8f2c7403c91e214d288c090a236074c
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-03-30 16:24:55 +0200

    Refactored the code of the temperature and window bars

--- src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/gui/TemperatureBar.java
+++ src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/gui/TemperatureBar.java
@@ -1,5 +1,7 @@
 package it.unibo.esiot.assignment03.dashboard.gui;
 
+import java.awt.Font;
+
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -7,6 +9,7 @@ import javax.swing.JPanel;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.esiot.assignment03.dashboard.controller.api.DashboardController;
+import it.unibo.esiot.assignment03.dashboard.model.states.TemperatureState;
 
 /**
  * Temperature management panel.
@@ -14,8 +17,17 @@ import it.unibo.esiot.assignment03.dashboard.controller.api.DashboardController;
 public final class TemperatureBar extends JPanel {
     private static final long serialVersionUID = 1L;
     private static final String DEGREE_SYMBOL = "\u00B0";
+    private static final Font FONT = new Font(Font.DIALOG, Font.LAYOUT_LEFT_TO_RIGHT, 18);
+    private static final Font BOLD_FONT = new Font(Font.DIALOG, Font.BOLD, 18);
 
     private final transient DashboardController controller;
+    private final JLabel curTemperatureLabel;
+    private final JLabel stateLabel;
+    private final JLabel avgTemperatureLabel;
+    private final JLabel minTemperatureLabel;
+    private final JLabel maxTemperatureLabel;
+    private final JButton alarmButton;
+
 
     /**
      * Creates a new temperature management panel.
@@ -29,15 +41,45 @@ public final class TemperatureBar extends JPanel {
     public TemperatureBar(final DashboardController controller) {
         this.controller = controller;
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.curTemperatureLabel = new JLabel();
+        this.stateLabel = new JLabel();
+        this.avgTemperatureLabel = new JLabel();
+        this.minTemperatureLabel = new JLabel();
+        this.maxTemperatureLabel = new JLabel();
+        this.alarmButton = new JButton("Manage Alarm");
+        this.setup();
+        this.draw();
+    }
 
-        this.add(new JLabel("Temperature: " + this.controller.getCurrentTemperature() + DEGREE_SYMBOL + "C"));
-        this.add(new JLabel("State: " + this.controller.getTemperatureState()));
-        this.add(new JLabel("Avg Temperature: " + this.controller.getAvgTemperature() + DEGREE_SYMBOL + "C"));
-        this.add(new JLabel("Min Temperature: " + this.controller.getMinTemperature() + DEGREE_SYMBOL + "C"));
-        this.add(new JLabel("Max Temperature: " + this.controller.getMaxTemperature() + DEGREE_SYMBOL + "C"));
+    private void setup() {
+        this.curTemperatureLabel.setFont(BOLD_FONT);
+        this.stateLabel.setFont(BOLD_FONT);
+        this.avgTemperatureLabel.setFont(FONT);
+        this.minTemperatureLabel.setFont(FONT);
+        this.maxTemperatureLabel.setFont(FONT);
+        this.alarmButton.setFont(FONT);
+        this.alarmButton.addActionListener(e -> this.controller.doManageAlarm());
 
-        final var button = new JButton("Manage Alarm");
-        button.addActionListener(e -> { });
-        this.add(button);
+        this.add(this.curTemperatureLabel);
+        this.add(this.stateLabel);
+        this.add(this.avgTemperatureLabel);
+        this.add(this.minTemperatureLabel);
+        this.add(this.maxTemperatureLabel);
+        this.add(this.alarmButton);
+    }
+
+    /**
+     * Displays temperature informations.
+     */
+    public void draw() {
+        this.curTemperatureLabel.setText("Temperature: " + this.controller.getCurrentTemperature() + DEGREE_SYMBOL + "C");
+        this.stateLabel.setText("State: " + this.controller.getTemperatureState());
+        this.avgTemperatureLabel.setText("Avg Temperature: " + this.controller.getAvgTemperature() + DEGREE_SYMBOL + "C");
+        this.minTemperatureLabel.setText("Min Temperature: " + this.controller.getMinTemperature() + DEGREE_SYMBOL + "C");
+        this.maxTemperatureLabel.setText("Max Temperature: " + this.controller.getMaxTemperature() + DEGREE_SYMBOL + "C");
+        this.alarmButton.setEnabled(this.controller.getTemperatureState().equals(TemperatureState.ALARM.toString()));
+
+        this.repaint();
+        this.validate();
     }
 }
--- src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/gui/WindowBar.java
+++ src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/gui/WindowBar.java
@@ -1,6 +1,7 @@
 package it.unibo.esiot.assignment03.dashboard.gui;
 
 import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -9,14 +10,22 @@ import javax.swing.JSlider;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.esiot.assignment03.dashboard.controller.api.DashboardController;
+import it.unibo.esiot.assignment03.dashboard.model.states.WindowMode;
 
 /**
  * Window management panel.
  */
 public final class WindowBar extends JPanel {
     private static final long serialVersionUID = 1L;
+    private static final Font FONT = new Font(Font.DIALOG, Font.LAYOUT_LEFT_TO_RIGHT, 18);
+    private static final Font BOLD_FONT = new Font(Font.DIALOG, Font.BOLD, 18);
 
     private final transient DashboardController controller;
+    private final JLabel modeLabel;
+    private final JButton switchModeButton;
+    private final JLabel openingLabel;
+    private final JSlider openingSlider;
+    private boolean sliderFlag;
 
     /**
      * Creates a new window management panel.
@@ -30,21 +39,43 @@ public final class WindowBar extends JPanel {
     public WindowBar(final DashboardController controller) {
         this.controller = controller;
         this.setLayout(new FlowLayout());
+        this.modeLabel = new JLabel();
+        this.switchModeButton = new JButton("Switch Mode");
+        this.openingLabel = new JLabel();
+        this.openingSlider = new JSlider();
+        this.setup();
+        this.draw();
+    }
 
-        this.add(new JLabel("Opening: " + this.controller.getWindowOpening() + "%"));
-        this.add(new JLabel("Mode: " + this.controller.getWindowMode()));
-
-        final var button = new JButton("Switch Mode");
-        final var slider = new JSlider();
-        button.addActionListener(e1 -> { });
-        this.add(button);
-
-        final var sliderValue = new JLabel();
-        sliderValue.setText(Integer.toString(slider.getValue()) + "%");
-        slider.addChangeListener(e2 -> {
-            sliderValue.setText(Integer.toString(slider.getValue()) + "%");
+    private void setup() {
+        this.modeLabel.setFont(BOLD_FONT);
+        this.switchModeButton.setFont(FONT);
+        this.openingLabel.setFont(BOLD_FONT);
+        this.openingSlider.setFont(FONT);
+        this.openingSlider.addChangeListener(e -> {
+            if (sliderFlag) {
+                this.controller.setOpeningFromDashboard(this.openingSlider.getValue());
+            }
         });
-        this.add(slider);
-        this.add(sliderValue);
+
+        this.add(this.modeLabel);
+        this.add(this.switchModeButton);
+        this.add(this.openingLabel);
+        this.add(this.openingSlider);
+    }
+
+    /**
+     * Displays window informations.
+     */
+    public void draw() {
+        this.modeLabel.setText("Mode: " + this.controller.getWindowMode());
+        this.openingLabel.setText("Opening: " + this.controller.getWindowOpening() + "%");
+        this.openingSlider.setEnabled(!this.controller.getWindowMode().equals(WindowMode.MANUAL.toString()));
+        this.sliderFlag = false; // NOPMD suppressed because used in slider change listener.
+        this.openingSlider.setValue(Integer.parseInt(this.controller.getWindowOpening()));
+        this.sliderFlag = true;
+
+        this.repaint();
+        this.validate();
     }
 }
