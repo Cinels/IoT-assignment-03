880d62cf416bdcc754c1ceb57ecc34c8544923df
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-04-08 14:33:17 +0200

    Added dashboard main function implementation

--- src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/App.java
+++ src/dashboard-frontend/src/main/java/it/unibo/esiot/assignment03/dashboard/App.java
@@ -1,5 +1,11 @@
 package it.unibo.esiot.assignment03.dashboard;
 
+import it.unibo.esiot.assignment03.dashboard.communication.ControlUnitCommunicationImpl;
+import it.unibo.esiot.assignment03.dashboard.controller.impl.CommunicationControllerImpl;
+import it.unibo.esiot.assignment03.dashboard.controller.impl.DashboardControllerImpl;
+import it.unibo.esiot.assignment03.dashboard.gui.Dashboard;
+import it.unibo.esiot.assignment03.dashboard.model.impl.SystemDataImpl;
+
 /**
  * The main class of the application, which starts its execution.
  */
@@ -10,5 +16,9 @@ public final class App {
      * The main method of the application, from which execution starts.
      * @param args are ignored.
      */
-    public static void main(final String[] args) { }
+    public static void main(final String[] args) {
+        final var data = new SystemDataImpl();
+        new ControlUnitCommunicationImpl(new CommunicationControllerImpl(data)).start();
+        new Dashboard(new DashboardControllerImpl(data)).start();
+    }
 }
